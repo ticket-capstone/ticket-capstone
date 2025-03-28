cd8e2420a1ef2fb987b9aa421fa7250ce87dbbca
양동현 <dongdong0516@naver.com>
2025-03-28 16:50:00 +0900

    project init

---
+++ src/test/java/com/capstone/ticketservice/TicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.ticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
