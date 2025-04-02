6352cf2be69d78535bfb19c3477ebb3e4d2e2673
양동현 <dongdong0516@naver.com>
2025-04-02 21:51:01 +0900

    수정

--- src/main/java/com/capstone/ticketservice/TicketServiceApplication.java
+++ src/main/java/com/capstone/ticketservice/TicketServiceApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class TicketServiceApplication {
-=======
-public class    TicketServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketServiceApplication.class, args);
