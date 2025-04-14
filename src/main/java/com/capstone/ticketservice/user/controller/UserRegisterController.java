a137924e5fcc350e1895c4bb15c222cb931b31a8
bcgntu <paul001093@gmail.com>
2025-04-14 21:03:27 +0900

    GT1 #회원가입, register.html 일부 수정

--- src/main/java/com/capstone/ticketservice/user/controller/UserRegisterController.java
+++ src/main/java/com/capstone/ticketservice/user/controller/UserRegisterController.java
@@ -1,9 +1,37 @@
 package com.capstone.ticketservice.user.controller;
 
+import com.capstone.ticketservice.user.dto.UserRegistrationDto;
+import com.capstone.ticketservice.user.model.Users;
+import com.capstone.ticketservice.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/users")
 public class UserRegisterController {
+    private final UserService userService;
+
+    @Autowired
+    public UserRegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/register")
+    public String registerPage(Model model) {
+        model.addAttribute("userRegistrationDto", new UserRegistrationDto());
+        return "user/register"; //user.register.html 반환
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Users> registerUser(@ModelAttribute("userRegistrationDto") UserRegistrationDto userRegistrationDto) {
+        Users registeredUser = userService.registerUser(userRegistrationDto);
+        return ResponseEntity.ok(registeredUser);
+    }
 }
