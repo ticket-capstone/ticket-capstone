2e42466501329c161bd20ea2efb21fb5377abb6b
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-05-08 15:01:10 +0900

    헤더와 푸터가 항상 보이도록 수정 2

--- src/main/java/com/capstone/ticketservice/user/controller/UserInfoController.java
+++ src/main/java/com/capstone/ticketservice/user/controller/UserInfoController.java
@@ -6,6 +6,8 @@ import com.capstone.ticketservice.user.service.UserInfoService;
 import com.capstone.ticketservice.user.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -68,7 +70,6 @@ public class UserInfoController {
     public String updateUserInfo(@PathVariable Long userid ,@ModelAttribute Users updatedUser, HttpSession session, Model model) {
 
         Users loggedInUser = (Users) session.getAttribute("user");
-        updatedUser = (Users) session.getAttribute("user"); //효율적이지 못하지만 각 역할에 맞도록 변수를 설정함
 
         // 로그인 유무 확인 후 아닐 경우 로그인 화면으로
         if (isNotAuthorized(session,userid)) {
@@ -83,6 +84,7 @@ public class UserInfoController {
         // 실제 정보 업데이트
         userInfoService.updateUserInfo(userid, updatedUser);
 
+        System.out.println(updatedUser.getName());
         // 세션 사용자 정보도 업데이트
         session.setAttribute("user", updatedUser);
 
@@ -95,23 +97,23 @@ public class UserInfoController {
 
     //사용자 탈퇴
     @DeleteMapping("/api/users/{userid}")
-    public String deleteUserInfo(@PathVariable Long userid, Model model , HttpSession session) {
+    public ResponseEntity<String> deleteUserInfo(@PathVariable Long userid, Model model , HttpSession session) {
 
         Users loggedInUser = (Users) session.getAttribute("user");
 
         // 로그인 유무 확인 후 아닐 경우 로그인 화면으로
         if (isNotAuthorized(session,userid)) {
-            return "redirect:/login";
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인이 필요합니다.");
         }
 
         // 다른 사용자 정보 접근 방지
         if (!loggedInUser.getUserId().equals(userid)) {
-            return "redirect:/"; // 홈 화면으로
+            return ResponseEntity.ok("접근이 불가 합니다"); // 홈 화면으로
         }
 
         userInfoService.deleteUserInfo(userid);
         session.invalidate(); //세션 만료 처리
-        return "redirect:/";
+        return ResponseEntity.ok("탈퇴 완료");
     }
 
 
