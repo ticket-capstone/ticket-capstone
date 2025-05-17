0d9d652e197cadcd77e767b9805a24b5771def99
양동현 <dongdong0516@naver.com>
2025-05-17 15:42:19 +0900

    feat: 이벤트 캐싱 기능 구현

--- src/main/java/com/capstone/ticketservice/event/service/EventService.java
+++ src/main/java/com/capstone/ticketservice/event/service/EventService.java
@@ -3,7 +3,9 @@ package com.capstone.ticketservice.event.service;
 import com.capstone.ticketservice.event.model.Event;
 import com.capstone.ticketservice.event.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,19 +13,19 @@ import java.util.List;
 public class EventService {
 
     private final EventRepository eventRepository;
-    //todo: sectionrepostiroy 나중에 주입받기 (좌석 정보)
 
     @Autowired
     public EventService(EventRepository eventRepository) {
         this.eventRepository = eventRepository;
     }
 
+    @Cacheable(value = "events")
     public List<Event> getEvents() {
         List<Event> events = eventRepository.findAll();
-
         return events;
     }
 
+    @Cacheable(value = "event", key = "#eventId")
     public Event getEventById(Long eventId) {
         return eventRepository.findById(eventId)
                 .orElseThrow(() -> new RuntimeException("이벤트를 찾을 수 없습니다."));
