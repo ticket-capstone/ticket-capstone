cab1dcb02e5cd9e9c472fc3ba97d8ebb006b9c6a
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-05-21 14:09:35 +0900

    REDIS를 활용한 대기열 큐

--- src/main/java/com/capstone/ticketservice/user/dto/LoginRequestDto.java
+++ src/main/java/com/capstone/ticketservice/user/dto/LoginRequestDto.java
@@ -1,14 +1,26 @@
 package com.capstone.ticketservice.user.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.capstone.ticketservice.user.model.Users;
+import lombok.*;
 
 @Getter
 @Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoginRequestDto {
 
     private String username;
     private String password;
+    private String role;
 
+    public static LoginRequestDto fromEntity(Users user) {
+        return LoginRequestDto.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .role(user.getRole().name())
+                .build();
+    }
 
 }
